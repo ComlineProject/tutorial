c6356a93a6525eb97dcd2815fc7f7216017a085f
Kinflou <ossdevlev@proton.me>
2024-01-29 23:46:30 +0000

    Trying out path resolving for deployment

--- src/lib/paging.ts
+++ src/lib/paging.ts
@@ -1,3 +1,5 @@
+import { dev } from "$app/environment"
+import { base } from "$app/paths"
 import { error } from "@sveltejs/kit"
 
 
@@ -8,12 +10,12 @@ export interface Page {
 }
 
 const PAGES: Page[] = [
-    { id: "p1", url: "/part-1_introduction", name: "Introduction" },
-    { id: "p1-s1", url: "/part-1_introduction/sect-1_what-is-comline", name: "What is Comline?" },
-    { id: "p1-s2", url: "/part-1_introduction/sect-2_how-is-comline", name: "How is Comline?" },
+    { id: "p1", url: "part-1_introduction", name: "Introduction" },
+    { id: "p1-s1", url: "part-1_introduction/sect-1_what-is-comline", name: "What is Comline?" },
+    { id: "p1-s2", url: "part-1_introduction/sect-2_how-is-comline", name: "How is Comline?" },
     
-    { id: "p2", url: "/part-2_basics", name: "Basics" },
-    { id: "p2-s1", url: "/part-2_basics/sect-2_what-is-comline", name: "What is Comline?" },
+    { id: "p2", url: "part-2_basics", name: "Basics" },
+    { id: "p2-s1", url: "part-2_basics/sect-2_what-is-comline", name: "What is Comline?" },
 ]
 
 export function pageUrl(id: string): string {
@@ -23,5 +25,10 @@ export function pageUrl(id: string): string {
         throw error(400, `Couldn't find tutorial page by ID '${id}', go back or start from beginning`)
     }
 
-    return found.url
+    const base_url = dev ? '' : base
+    const url = `${base_url}/${found.url}`
+    
+    console.log(url)
+
+    return url
 }
